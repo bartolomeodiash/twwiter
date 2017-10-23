eb61aea3d52e768bb31c3b5556e2fa415a805b6a
ggaleon <ss.sheyko@gmail.com>
2017-10-23 19:12:51 +0300

    Initial commit

---
+++ src/main/java/ua/slava/twit/App.java
@@ -0,0 +1,13 @@
+package ua.slava.twit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Its my twitter story!" );
+    }
+}
